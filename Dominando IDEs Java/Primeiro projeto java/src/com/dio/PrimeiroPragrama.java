5d1192ef6c6fd1a8213020520a1337cfe5617ca4
Egle-Farias <suporto.eglefarias@gmail.com>
2022-09-25 17:48:35 -0300

    Primeiro_Projeto

---
+++ Dominando IDEs Java/Primeiro projeto java/src/com/dio/PrimeiroPragrama.java
@@ -0,0 +1,22 @@
+package com.dio;
+
+public class PrimeiroPragrama {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 2;
+		int b = 3; 
+              System.out.println("Helloword " + (a + b));
+              int total = (a + b);
+              
+              String nome = "Paulo"; 
+              System.out.println(nome);
+            int agencia = 01;
+            int conta = 23125;
+            int digito = 9;
+            
+            System.out.println(total);
+            
+	}
+
+}
